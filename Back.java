35c484e74cfa22e067af63bfa04ebb625ab4c2f1
rupsikha <rupsikha007@gmail.com>
2016-09-29 15:33:21 +0530

    Add files via upload

---
+++ Back.java
@@ -0,0 +1,9 @@
+public class Back
+{
+public String backAround(String str)
+{
+int l=str.length();
+l--;
+return (str.charAt(l)+str);
+}
+}
---
+++ Solution6.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Solution6
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+String s=in.next();
+Back b=new Back();
+String st=b.backAround(s);
+System.out.println(st);
+}
+}
